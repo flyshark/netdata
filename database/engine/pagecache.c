8d490ff5103a683e6028de397e55f007679be0bf
Stelios Fragkakis <52996999+stelfrag@users.noreply.github.com>
2022-05-05 12:32:12 +0300

    Add 3 retries on page lookup failure

--- database/engine/pagecache.c
+++ database/engine/pagecache.c
@@ -1067,10 +1067,14 @@ pg_cache_lookup_next(struct rrdengine_instance *ctx, struct pg_cache_page_index 
 
     page_not_in_cache = 0;
     uv_rwlock_rdlock(&page_index->lock);
+    int retry_count = 0;
     while (1) {
         descr = find_first_page_in_time_range(page_index, start_time, end_time);
-        if (NULL == descr || 0 == descr->page_length) {
+        if (NULL == descr || 0 == descr->page_length || retry_count == 3) {
             /* non-empty page not found */
+            if (retry_count == 3 && descr == NULL) {
+                error_report("Page cache timeout while waiting for page %p : returning FAIL", descr);
+            }
             uv_rwlock_rdunlock(&page_index->lock);
 
             pg_cache_release_pages(ctx, 1);
@@ -1115,8 +1119,10 @@ pg_cache_lookup_next(struct rrdengine_instance *ctx, struct pg_cache_page_index 
         if (!(flags & RRD_PAGE_POPULATED))
             page_not_in_cache = 1;
 //        pg_cache_wait_event_unsafe(descr);
-        if (pg_cache_timedwait_event_unsafe(descr, 1) == UV_ETIMEDOUT)
-            error_report("Page cache timeout while waiting for page %p", descr);
+        if (pg_cache_timedwait_event_unsafe(descr, 1) == UV_ETIMEDOUT) {
+            error_report("Page cache timeout while waiting for page %p : retry count = %d", descr, retry_count);
+            ++retry_count;
+        }
         rrdeng_page_descr_mutex_unlock(ctx, descr);
 
         /* reset scan to find again */
